7108e682b1145c4255f835a59e8ad90c67c315a6
tell-k <tell_k@tell-k-mac.local>
2010-03-06 15:35:05 +0900

    commit scheme file

---
+++ scheme/str_reverse.scm
@@ -0,0 +1,14 @@
+(define (list-reverse ls)
+ (letrec
+  ((rev (lambda (ret l)
+         (if (null? l)
+          ret
+          (rev (cons (car l) ret)
+           (cdr l))))))
+
+  (rev '() ls)))
+
+(define (str_reverse str)
+    (list->string (list-reverse (string->list str))))
+
+(print (str_reverse "あいうえお日本語"))
