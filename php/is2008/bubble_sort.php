a73d4598adfff7443ae6e3bfdb7e72c21563f5d1
tell-k <ffk2005@gmail.com>
2010-07-18 01:57:45 +0900

    * commit

--- php/is2008/bubble_sort.php
+++ php/is2008/bubble_sort.php
@@ -2,22 +2,19 @@
 
 $hoge = array(0, 1, 2, 3, 4, 5);
 function bubble_sort($hoge, $order='asc'){
-    for($i=0;$i<count($hoge);$i++)
-    {
-      for($j=count($hoge)-1;$j>$i;$j--)
-      {
+
+    for($i=0;$i<count($hoge);$i++) {
+      for($j=count($hoge)-1;$j>$i;$j--) {
         switch($order){
           case 'asc':
-            if($hoge[$i] > $hoge[$j])
-            { 
+            if($hoge[$i] > $hoge[$j]) { 
               $tmp = $hoge[$i];
               $hoge[$i] = $hoge[$j];
               $hoge[$j] = $tmp;
             }
             break;
           case 'desc':
-            if($hoge[$i] < $hoge[$j])
-            {
+            if($hoge[$i] < $hoge[$j]) {
               $tmp = $hoge[$i];
               $hoge[$i] = $hoge[$j];
               $hoge[$j] = $tmp;
--- php/is2008/prime_1.php
+++ php/is2008/prime_1.php
@@ -1,13 +1,13 @@
 <?php
+//素数判定
 
-for($i=0; $i<100; $i++){
+for($i=0; $i<100; $i++) {
     echo (is_prime($i+1))? "prime => ".($i+1)."\n" : "not prime => ".($i+1)."\n";
 }
 
-function is_prime($num)
-{
-  if($num < 2) return false;
-  for($i=2;$i<$num;$i++){if(!($num%$i)){return false;}}
+function is_prime($num) {
+  if ($num < 2) return false;
+  for ($i = 2; $i < $num; $i++){ if(!($num%$i)){ return false;}}
   return true;
 }
 
