7cfb2a9041fbe0d4b78115db30fe6f6f0f0c247f
tell-k <ffk2005@gmail.com>
2010-08-19 14:48:15 +0900

    * commit

--- php/function_return_ref.php
+++ php/function_return_ref.php
@@ -1,1 +1,22 @@
 <?php
+
+//ref http://d.hatena.ne.jp/cloned/20060510
+class Hoge
+{
+    public static $value = "default";
+    public static function &getValue()
+    {
+        return self::$value;
+    }
+}
+class Fuga
+{
+    public static function &getValue()
+    {
+        $value =& Hoge::getValue();
+        return $value;
+    }
+}
+$v =& Fuga::getValue();
+$v = "HelloWorld";
+print Hoge::$value;
