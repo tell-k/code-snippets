79c1cef95d7553878d02b9a644de0ee6fd463e86
tell-k <ffk2005@gmail.com>
2011-04-01 16:14:43 +0900

    commit

--- scheme/fib.scm
+++ scheme/fib.scm
@@ -6,8 +6,8 @@
      (+ (fib (- n 1))
         (fib (- n 2)))))
 
-(print (fib 1)) ;=> 1
 (print (fib 0)) ;=> 1
+(print (fib 1)) ;=> 1
 (print (fib 2)) ;=> 2
 (print (fib 3)) ;=> 3
 (print (fib 4)) ;=> 5
