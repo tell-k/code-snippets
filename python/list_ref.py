c3bf9df70204e38f990f6063561f028097121133
tell-k <ffk2005@gmail.com>
2011-05-16 19:06:37 +0900

    commit

---
+++ python/list_ref.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python
+#-*- coding:utf8 -*-
+
+a = ['bucho', 'bucho2', 'bucho3']
+b = a
+a.append('bucho4')
+print b #=> ['bucho', 'bucho2', 'bucho3', 'bucho4']
---
+++ python/namespace/namespace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+import sys
+sys.path.insert(0, './bucho2/')
+sys.path.insert(0, './bucho/')
+import lover
+print lover.ae35()
+print lover.shin()
