080924b964015dd97b1f421fccd51c28516652f8
tell-k <ffk2005@gmail.com>
2011-07-23 20:59:27 +0900

    commit

--- python/facebook/create_test_user.py
+++ python/facebook/create_test_user.py
@@ -9,21 +9,50 @@
 """ 
 
 import urllib
+import cgi
 import simplejson
 import StringIO
 
+#set your app_id, consumer_secret
+APP_ID = ""
+CONSUMER_SECRET = ""
 
-app_id = current_app.config['FACEBOOK_APP_ID']
-fb = FacebookAPI(app_id, current_app.config['FACEBOOK_CONSUMER_SECRET'])
-app_token = fb.get_app_access_token()
+#num of test user
+TEST_USER_NUM = 2
 
-url = "https://graph.facebook.com/" + app_id + "/accounts/test-users?" + urllib.urlencode(dict(
-                            installed='false',
-                            name='testbucho3',
-                            method='post',
-                            access_token=app_token,
-                            ))
-res = simplejson.load(StringIO.StringIO(urllib.urlopen(url).read()))
-    for k in res.keys():
-        print k + ': ' + res[k]
+def get_app_access_token(app_id, consumer_secret):
+    """ 登録Facebookアプリ自体のアクセストークンを取得 """
+    url = "https://graph.facebook.com/oauth/access_token?" + urllib.urlencode(dict(
+        client_id=app_id,
+        client_secret=consumer_secret,
+        grant_type="client_credentials",
+    ))
+    response = cgi.parse_qs(urllib.urlopen(url).read())
+    access_token = response["access_token"][-1]
+    return access_token
 
+def get_test_user(app_id, app_token, installed, name):
+    """ test user の取得"""
+    base_url = "https://graph.facebook.com/" + app_id + "/accounts/test-users?"
+    url = base_url + urllib.urlencode(dict(
+                                installed=installed,
+                                name=name,
+                                method='post',
+                                access_token=app_token,
+                                ))
+    return simplejson.load(StringIO.StringIO(urllib.urlopen(url).read()))
+
+def make_friends(app_id, app_token, installed, name):
+    pass
+
+if __name__ == '__main__':
+    test_user_ids = {}
+
+    app_token = get_app_access_token(APP_ID, CONSUMER_SECRET)
+
+    for i in range(TEST_USER_NUM):
+        test_user = get_test_user(APP_ID, app_token, 'false', 'testbucho' + str(i))
+        for k in test_user.keys():
+            print k + ': ' + test_user[k]
+            if k == 'id':
+                test_user_ids.append(test_user[k])
