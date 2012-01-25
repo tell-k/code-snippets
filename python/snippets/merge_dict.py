5ec37f2e9e8de15d97d3ee962c0a9c6c48fb60eb
tell-k <ffk2005@gmail.com>
2012-01-25 21:38:42 +0900

    commit

--- python/snippets/merge_dict.py
+++ python/snippets/merge_dict.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 #-*- coding:utf8 -*-
+from collections import Counter
 
+d1 = {'a': 1, 'c': 3, 'b': 2}
+d2 = {'a': 1, 'c': -3, 'b': -3}
 
-a = {"test1": 2, "test2": 2}
-b = {"test1": 2, "test3": 2}
+def merge_dict_add_values(d1, d2):
+    return dict(Counter(d1) + Counter(d2))
 
+def merge_dict(d1, d2, func=lambda x, y: y):
+    d1 = d1.copy()
+    d2 = d2.copy()
+    for k, v in d2.iteritems():
+        d1[k] = func(d1[k], v) if k in d1 else v
+    d2.update(d1)
+    return d2
+
+print merge_dict_add_values(d1, d2) #=> {'a': 2}
+print merge_dict(d1, d2, lambda x, y: x + y) #=> {'a': 2, 'c': 0, 'b': -1}
 
